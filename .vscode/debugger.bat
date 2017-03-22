7cdae545598096c2ad3ece94e69d880bd2319eaf
kamzab95 <drazz3r@gmail.com>
2017-03-22 11:09:15 +0100

    Delete debugger.bat

--- .vscode/debugger.bat
+++
@@ -1,5 +0,0 @@
-set PATH=C:\Users\drazz\.vscode\HusarionTools\bin\;%PATH%
-cd c:\Users\drazz\OneDrive\Dokumenty\VSCode\husarion || exit 1
-start /wait st-flash write myproject.bin 0x08010000 || exit 1
-start st-util
-arm-none-eabi-gdb %*
